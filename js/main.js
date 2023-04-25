8bef8257801b7fd40f15137ccc4eefcfdf9ea5d3
Arkadiusz <franasze@student.wszib.edu.pl>
2023-04-25 23:34:10 +0200

    Game ready

--- js/main.js
+++ js/main.js
@@ -1,34 +1,10 @@
-setInterval(() =>{
-  const game = document.querySelector("#game");
+const endGame = new EndGame();
 
+const game = new Game(endGame);
 
-  const egg = new Image();
-  egg.addEventListener("load", () => {
-    egg.classList.add("egg");
-    const x = Math.round(Math.random() * (120 - (5 * egg.width / egg.height)));
-    egg.style.left = x + "vh";
-    game.appendChild(egg)
-  });
+endGame.game = game;
 
-  egg.src = "images/egg-69303_1280.jpg"
-},1000);
-
-setInterval(() => {
-  document.querySelectorAll("#game > .egg").forEach(element => {
-    const top = element.style.top.length !== 0 ? parseFloat(element.style.top) : 0;
-    console.log(element.style.top);
-    element.style.top = (top + 0.03) + "vh";
-
-    if (top > 100) {
-      element.remove();
-
-      const heart = document.querySelector(".heart");
-      heart.remove();
-      if (!document.querySelector("heart")) {
-        alert("Game Over!");
-        window.location.reload();
-      }
-    }
-  })
-}, 30 / 1000);
-
+document.querySelector("#start-game > button").addEventListener("click", () => {
+  document.querySelector("#start-game").style.display = "none";
+  game.start();
+})
